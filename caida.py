59688a3933fd5d220ae7ecba14c1a89b77be9177
Fabian Barajas <fabiancamilobar@outlook.com>
2020-03-03 13:53:36 -0500

    ejercicios

---
+++ caida.py
@@ -0,0 +1,7 @@
+print('caida libre')
+g=float(input('digite la gravedad: '))
+t=float(input('digite time: '))
+y_0=float(input('digite altura inicial: '))
+y=y_0-0.5*g*t**2
+print('la altura es: ', y)
+
